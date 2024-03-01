7ef444664c0d777ad41bfca1409864c84150fa74
GabriiCoder <150010585+GabriiCoder@users.noreply.github.com>
2024-03-01 14:32:39 +0100

    created login

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -10,28 +11,46 @@ use Symfony\Contracts\Service\Attribute\Required;
 
 class UserController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request)
+    {
         $request->validate([
-            'name'=>'required',
-            'email'=>'required|email|unique:users',
-            'password'=>'required|confirmed'
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
         ]);
 
         $user = new User();
         $user->name->$request->name;
         $user->email->$request->email;
-        $user->password=Hash::make($request->password);
+        $user->password = Hash::make($request->password);
 
 
         $user->save();
-        
-        return response()->json([
-            'status'=>1, 
-            'msg'=>'Registrado correctamente'
-        ]);
-        
-        
 
+        return response()->json([
+            'status' => 1,
+            'msg' => 'Registrado correctamente'
+        ]);
     }
 
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', '=', $request->email)->first();
+
+        if (isset($user->id)) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('auth_token')->plainTextToken;
+            }
+            return response()->json([
+                'status' => 1,
+                'msg' => 'Inicio de sesión correctamente',
+                'access_token' => $token
+            ]);
+        };
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);

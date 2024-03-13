7e05a5b7f46433963e4447ee7c0884715a4e723b
ImLauraGS <lgilsolano@gmail.com>
2024-03-13 18:50:07 +0100

    hotfix reguster errors added

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,44 +6,51 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        try{
-        $request->validate([
-            'name' => 'required|string|max:150|min:5',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|string|min:5'
-        ]);
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:150|min:5',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:5'
+            ], [
+                'name.required' => 'Nombre requerido',
+                'name.min' => 'Nombre debe tener al menos 5 caracteres.',
+                'email.required' => 'E-mail requerido',
+                'email.email' => 'El correo electrónico debe ser válido.',
+                'email.unique' => 'Este email ya está registrado.',
+                'password.required' => 'Contraseña requerida',
+                'password.min' => 'La contraseña debe tener al menos 5 caracteres.'
+            ]);
 
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-
-
-        $user->save();
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-
-        return response()->json([
-            'status' => 1,
-            'msg' => 'Registrado correctamente',
-            'access_token' => $token
-
-        ],201);
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            if ($e->errors()['email'][0] === 'The email has already been taken.') {
-                return response()->json(['status' => 0, 'msg' => 'El correo electrónico ya está en uso'], 400);
-            } else {
-                return response()->json(['status' => 0, 'msg' => 'Error al registrar el usuario'], 500);
+            if ($validator->fails()) {
+                return response()->json(['status' => 0, 'msg' => 'Error al registrar el usuario', 'errors' => $validator->errors()], 400);
             }
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'status' => 1,
+                'msg' => 'Registrado correctamente',
+                'access_token' => $token
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['status' => 0, 'msg' => 'Error al registrar el usuario'], 500);
         }
     }
+
+
 
     public function login(Request $request)
     {   
